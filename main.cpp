35b503146213bcb67fd44740c60065b46bf43644
Mateusz Broniszewski <mateusz.broniszewski.programista@gmail.com>
2019-06-08 11:49:55 +0200

    Created base class KsiazkaAdresowa and Uzytkownik

--- main.cpp
+++ main.cpp
@@ -1,9 +1,13 @@
 #include <iostream>
 
+#include "KsiazkaAdresowa.h"
+
+
 using namespace std;
 
 int main()
 {
-    cout << "Hello world!" << endl;
+    KsiazkaAdresowa ksiazkaAdresowa;
+
     return 0;
 }
