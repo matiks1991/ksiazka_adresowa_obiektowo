e49c4f583c54bd56ac56149aebffdc9eefdaa1fd
Mateusz Broniszewski <mateusz.broniszewski.programista@gmail.com>
2019-06-09 23:42:05 +0200

    Add registered user to vector

--- main.cpp
+++ main.cpp
@@ -8,6 +8,9 @@ using namespace std;
 int main()
 {
     KsiazkaAdresowa ksiazkaAdresowa;
+    ksiazkaAdresowa.rejestracjaUzytkownika();
+    ksiazkaAdresowa.rejestracjaUzytkownika();
+    ksiazkaAdresowa.wypiszWszystkichUzytkownikow();
 
     return 0;
 }
